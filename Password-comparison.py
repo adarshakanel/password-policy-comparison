0d7f6ba6e9be61534bf7bbecbdd2a5d9f4bd080a
adarshakanel <52978035+adarshakanel@users.noreply.github.com>
2021-11-10 18:35:49 -0700

    update1

--- Password-comparison.py
+++ Password-comparison.py
@@ -1,13 +1,22 @@
 from zxcvbn import zxcvbn
-results = zxcvbn('JohnSmith123', user_inputs=['John', 'Smith'])
 
-# y = json.loads(results)
-sequence = results["sequence"]
 
-guesses = sequence[0]["guesses"]
-token = sequence[0]["token"]
-rank = sequence[0]["rank"]
-base_guesses = sequence[0]["base_guesses"]
+def zxcvbn_result(input):
+    results = zxcvbn('JohnSmith123', user_inputs=['John', 'Smith'])
+    sequence = results["sequence"]
+    guesses = sequence[0]["guesses"]
+    token = sequence[0]["token"]
+    rank = sequence[0]["rank"]
+    base_guesses = sequence[0]["base_guesses"]
+    print("token: ", token, ", rank ", rank,
+          ", guesses: ", guesses, ", base_guesses ", base_guesses)
 
-print("token: ", token, " rank ", rank,
-      " guesses: ", guesses, " base_guesses ", base_guesses)
+
+def compare(files):
+    for file in files:
+        for input in file:
+            print(input)
+            zxcvbn_result(input)
+
+
+compare([["password1", "password2"], ["password3", "password4"]])
