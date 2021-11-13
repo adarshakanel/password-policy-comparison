e6bddc02c806aa36abb7d10fda4ad2b863861808
adarshakanel <52978035+adarshakanel@users.noreply.github.com>
2021-11-13 00:38:13 -0700

    reading password files and parsing

--- Password-comparison.py
+++ Password-comparison.py
@@ -1,22 +1,35 @@
 from zxcvbn import zxcvbn
+file1 = open("1-Google.txt")
+createNewFile1 = open("1-Google-zxcvbn.txt", "w")
 
 
-def zxcvbn_result(input):
-    results = zxcvbn('JohnSmith123', user_inputs=['John', 'Smith'])
-    sequence = results["sequence"]
-    guesses = sequence[0]["guesses"]
-    token = sequence[0]["token"]
-    rank = sequence[0]["rank"]
-    base_guesses = sequence[0]["base_guesses"]
-    print("token: ", token, ", rank ", rank,
-          ", guesses: ", guesses, ", base_guesses ", base_guesses)
+def zxcvbn_result(line):
+    try:
+        results = zxcvbn(line, user_inputs=[])
+        print(results)
+        sequence = results["sequence"]
+        score = results["score"]
+        guess = sequence[0]["guesses"]
+        pattern = sequence[0]["pattern"]
+        print("sequence: ", sequence)
+        print("guess: ", guess)
+        print("pattern: ", pattern)
+        print("score: ", score, "\n")
+    except:
+        return print("Not Happening\n")
 
 
 def compare(files):
-    for file in files:
-        for input in file:
-            print(input)
-            zxcvbn_result(input)
+    # for file in files:
+    count = 0
+    for line in files:
+        # to test
+        if count > 500:
+            break
+        else:
+            count += 1
+        # print(input)
+        zxcvbn_result(line)
 
 
-compare([["password1", "password2"], ["password3", "password4"]])
+compare(file1)
